62f267bc6538f75c4729b62f7f9320c34a0318cb
Alexandr Semenov <133553954+AlexSemen@users.noreply.github.com>
2023-11-24 04:28:45 +0300

    .

---
+++ Assets/Scripts/AnimatorPlayerController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorPlayerController : MonoBehaviour
+{
+    public static class Params
+    {
+        public const string Speed = "Speed";
+    }
+}
+
